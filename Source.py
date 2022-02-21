629d68839ca72bf0e0f588d1c62af1c8e8981ea0
KartikeyJadli <96066261+KartikeyJadli@users.noreply.github.com>
2022-02-21 09:27:27 +0530

    Add files via upload

--- Source.py
+++ Source.py
@@ -1,129 +1,76 @@
-# Byte-compiled / optimized / DLL files
-__pycache__/
-*.py[cod]
-*$py.class
+import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
 
-# C extensions
-*.so
+# Funcyion to return index by taking movie names
+def get_title_from_index(index):
+    try: 
+        return (df[df.index == index]["title"].values[0])
+    except Exception as e:
+        print("This movie name is not in our data set and check your spelling.\n ")
+        exit(0)
+# Function to return movies name by taking index
+def get_index_from_title(title):
+    try:
+        return df[df.title == title]["index"].values[0]
+    except Exception as e:
+        print("This movie name is not in our data set and check your spelling.\n ")
+        exit(0)
+# Read CSV File
+df = pd.read_csv("https://raw.githubusercontent.com/Diya-1/movie_recomendation/main/movie_dataset.csv")
+#print df.columns
 
-# Distribution / packaging
-.Python
-build/
-develop-eggs/
-dist/
-downloads/
-eggs/
-.eggs/
-lib/
-lib64/
-parts/
-sdist/
-var/
-wheels/
-pip-wheel-metadata/
-share/python-wheels/
-*.egg-info/
-.installed.cfg
-*.egg
-MANIFEST
+# Select Features by which we want to recommend mpovies 
 
-# PyInstaller
-#  Usually these files are written by a python script from a template
-#  before PyInstaller builds the exe, so as to inject date/other infos into it.
-*.manifest
-*.spec
+features = ['keywords','cast','genres','director']
 
-# Installer logs
-pip-log.txt
-pip-delete-this-directory.txt
 
-# Unit test / coverage reports
-htmlcov/
-.tox/
-.nox/
-.coverage
-.coverage.*
-.cache
-nosetests.xml
-coverage.xml
-*.cover
-*.py,cover
-.hypothesis/
-.pytest_cache/
+# Function to fill the missing values in our dataset
+for feature in features:
+	df[feature] = df[feature].fillna('')
 
-# Translations
-*.mo
-*.pot
+# Create a column in DF which combines all selected features
+def combine_features(row):
+	try:
+		return row['keywords'] +" "+row['cast']+" "+row["genres"]+" "+row["director"]
+	except:
+		print ("Error:", row)	
+df["combined_features"] = df.apply(combine_features,axis=1)
 
-# Django stuff:
-*.log
-local_settings.py
-db.sqlite3
-db.sqlite3-journal
+# To check the combine function 
+#print("Combined Features:", df["combined_features"].head())
 
-# Flask stuff:
-instance/
-.webassets-cache
+# Creating count matrix from this new combined column
+cv = CountVectorizer()
 
-# Scrapy stuff:
-.scrapy
+count_matrix = cv.fit_transform(df["combined_features"])
 
-# Sphinx documentation
-docs/_build/
+# Compute the Cosine Similarity based on the count_matrix
+cosine_sim = cosine_similarity(count_matrix) 
 
-# PyBuilder
-target/
+# Taking the inputs from the user
+movie_user_likes = input("Enter the name ofg movie by which you want to recommendation:")
+movie_user_likes=movie_user_likes.title()
+no_movies_recommend=int(input("Enter the number of movies you want to recommend: "))
 
-# Jupyter Notebook
-.ipynb_checkpoints
+# Get index of this movie from its title
+movie_index = get_index_from_title(movie_user_likes)
 
-# IPython
-profile_default/
-ipython_config.py
+# Creating the list of similar movies
+similar_movies =  list(enumerate(cosine_sim[movie_index]))
 
-# pyenv
-.python-version
+# Get a list of similar movies in descending order of similarity score
+sorted_similar_movies = sorted(similar_movies,key=lambda x:x[1],reverse=True)
 
-# pipenv
-#   According to pypa/pipenv#598, it is recommended to include Pipfile.lock in version control.
-#   However, in case of collaboration, if having platform-specific dependencies or dependencies
-#   having no cross-platform support, pipenv may install dependencies that don't work, or not
-#   install all needed dependencies.
-#Pipfile.lock
-
-# PEP 582; used by e.g. github.com/David-OConnor/pyflow
-__pypackages__/
-
-# Celery stuff
-celerybeat-schedule
-celerybeat.pid
-
-# SageMath parsed files
-*.sage.py
-
-# Environments
-.env
-.venv
-env/
-venv/
-ENV/
-env.bak/
-venv.bak/
-
-# Spyder project settings
-.spyderproject
-.spyproject
-
-# Rope project settings
-.ropeproject
-
-# mkdocs documentation
-/site
-
-# mypy
-.mypy_cache/
-.dmypy.json
-dmypy.json
-
-# Pyre type checker
-.pyre/
+# Print titles of  movies
+i=0
+try:
+    for element in sorted_similar_movies:
+        print(get_title_from_index(element[0]))
+        i=i+1
+        if (i==no_movies_recommend):
+            break
+except Exception as e:
+    print("We can recommend only these movies.\n")
+print("\n\t\tTHANK YOU.")
